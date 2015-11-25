a4e58183e76e446dd1c62628d50a84b5e212fcd5
diegomvh <diegomvh@gmail.com>
2015-11-25 12:57:37 -0300

    Add Stj.Email for Postal and RazorEngine Extensions

---
+++ Infrastructure/Stj.Email/RazorEngine/TemplateManager.cs
@@ -0,0 +1,23 @@
+﻿using RazorEngine.Templating;
+using System;
+
+namespace Stj.Email.RazorEngine
+{
+    public class TemplateManager : ITemplateManager
+    {
+        public void AddDynamic(ITemplateKey key, ITemplateSource source)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITemplateKey GetKey(string name, ResolveType resolveType, ITemplateKey context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITemplateSource Resolve(ITemplateKey key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
