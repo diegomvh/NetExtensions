7371641d394fb1f9aaec00d2450f27b804267e0b
diegomvh <diegomvh@gmail.com>
2016-02-23 10:06:24 -0300

    New pdf converter for string and razor

---
+++ Infrastructure/Stj.Utilities/Pdf/PdfConverter.cs
@@ -0,0 +1,106 @@
+﻿namespace Stj.Utilities.Pdf
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Text;
+    using System.Web.Mvc;
+
+    using Common.Logging;
+
+    public class PdfConverter
+    {
+        private readonly string HtmlToPdfExePath;
+        private readonly ILog log = LogManager.GetLogger(typeof(PdfConverter));
+
+        public PdfConverter(string htmlToPdfExePath = "wkhtmltopdf.exe") {
+            this.HtmlToPdfExePath = htmlToPdfExePath;
+        }
+
+        public byte[] FromString(string source)
+        {
+            Process p;
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = this.HtmlToPdfExePath;
+            psi.WorkingDirectory = Path.GetDirectoryName(psi.FileName);
+
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardInput = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+
+            //Argumentos http://wkhtmltopdf.org/usage/wkhtmltopdf.txt
+            string args = "-q -n ";
+            args += "--disable-smart-shrinking ";
+            args += "--outline-depth 0 ";
+            args += "--page-size A4 ";
+            args += " - -";
+
+            psi.Arguments = args;
+
+            p = Process.Start(psi);
+
+            try
+            {
+                using (StreamWriter stdin = p.StandardInput)
+                {
+                    stdin.AutoFlush = true;
+                    var b = Encoding.UTF8.GetBytes(source);
+                    stdin.BaseStream.Write(b, 0, b.Length);
+                }
+
+                byte[] buffer = new byte[32768];
+                byte[] file;
+                using (var ms = new MemoryStream())
+                {
+                    while (true)
+                    {
+                        int read = p.StandardOutput.BaseStream.Read(buffer, 0, buffer.Length);
+                        if (read <= 0)
+                            break;
+                        ms.Write(buffer, 0, read);
+                    }
+                    file = ms.ToArray();
+                }
+
+                p.StandardOutput.Close();
+                p.WaitForExit(60000);
+
+                int returnCode = p.ExitCode;
+                p.Close();
+
+                if (returnCode == 0)
+                    return file;
+                else
+                    log.Error("Could not create PDF, returnCode:" + returnCode);
+            }
+            catch (Exception ex)
+            {
+                log.Error("Could not create PDF", ex);
+            }
+            finally
+            {
+                p.Close();
+                p.Dispose();
+            }
+            return null;
+        }
+
+        public byte[] FromRazorView(string template, string applicationPathRoot, string viewPathRoot, IDictionary<string, object> values)
+        {
+            var engine = new Stj.Utilities.RazorEngine.FileSystemRazorViewEngine(applicationPathRoot, viewPathRoot);
+            var controller_context = new ControllerContext();
+            var view = engine.FindView(controller_context, template, null, true);
+            using (var writer = new StringWriter())
+            {
+                var context = new ViewContext(controller_context, view.View,
+                    new ViewDataDictionary(values), new TempDataDictionary(), writer);
+                view.View.Render(context, writer);
+                writer.Flush();
+                return this.FromString(writer.ToString());
+            }
+        }
+    }
+}
