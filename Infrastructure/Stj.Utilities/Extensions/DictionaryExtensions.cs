381c02dfd00e2fb4e1ed2626c84764b8864b8b6f
diegomvh <diegomvh@gmail.com>
2016-07-13 07:46:08 -0300

    More extensions

---
+++ Infrastructure/Stj.Utilities/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Stj.Utilities.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static V SetDefault<K, V>(this IDictionary<K, V> dict, K key, V @default)
+        {
+            V value;
+            if (!dict.TryGetValue(key, out value))
+            {
+                dict.Add(key, @default);
+                return @default;
+            }
+            else
+            {
+                return value;
+            }
+        }
+    }
+}
---
+++ Infrastructure/Stj.Utilities/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stj.Utilities.Extensions
+{
+    static public class EnumerableExtensions
+    {
+        static public IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> knownKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (knownKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
+    }
+}
